818d70b7172b569ba4e57845e4ad1bfa9d0198f3
sasham43 <sashasemail@gmail.com>
2016-07-24 16:00:53 -0500

    file structure created, angular loaded and server servering

---
+++ client/client.js
@@ -0,0 +1,7 @@
+angular.module('longplayApp', []);
+
+angular.module('longplayApp').controller('IndexController', ['$http', function($http){
+  var ic = this;
+
+  console.log('index controller loaded. ');
+}]);
